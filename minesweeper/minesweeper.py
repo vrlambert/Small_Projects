2b1fbd516b9605bc6576ad6ade206f8bc3716356
Victor Lambert <vrlambert@gmail.com>
2017-12-28 14:13:13 -0500

    Chore: rename bombs to mines

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -7,42 +7,42 @@ By Victor Lambert
 import random
 
 class Board(object):
-    def __init__(self, bombs = 10, size = (10,10)):
+    def __init__(self, mines = 10, size = (10,10)):
         # extract the size for use later
         self.size_x = size[0]
         self.size_y = size[1]
 
         # save the number of bombs
-        self.bomb_count = bombs
+        self.mine_count = mines
 
         # initialize a zero array for holding the bomb locations
-        self.bomb_board = [[0 for _ in range(self.size_x)]
+        self.mine_board = [[0 for _ in range(self.size_x)]
                                                     for _ in range(self.size_y)]
         # initialize a board to hold the square numbers
         self.number_board = [[0 for _ in range(self.size_x)]
                                                     for _ in range(self.size_y)]
         # Set the bomb locations
-        self.set_bombs()
+        self.set_mines()
         self.set_numbers()
         self.display_hidden()
 
-    def set_bombs(self):
+    def set_mines(self):
         # Initialize a bomb counter
-        bombs = 0
+        mines = 0
 
         # Set random locations until the number of bombs is met
-        while bombs < self.bomb_count:
+        while mines < self.mine_count:
             rand_x = random.randint(0, self.size_x - 1)
             rand_y = random.randint(0, self.size_y - 1)
-            if self.bomb_board[rand_y][rand_x] != 1:
+            if self.mine_board[rand_y][rand_x] != 1:
                 # 1 in the bomb board means bomb
-                self.bomb_board[rand_y][rand_x] = 1
+                self.mine_board[rand_y][rand_x] = 1
                 # -1 in number board means bomb
                 self.number_board[rand_y][rand_x] = -1
-                bombs += 1
+                mines += 1
 
     def set_numbers(self):
-        for j, row in enumerate(self.bomb_board):
+        for j, row in enumerate(self.mine_board):
             for i, bomb in enumerate(row):
                 if bomb == 1:
                     for neighbor in self.get_neighbors((i, j)):
@@ -63,7 +63,7 @@ class Board(object):
         return res
 
     def display_hidden(self):
-        for row in self.bomb_board:
+        for row in self.mine_board:
             print row
         print '-------------'
         for row in self.number_board:
